508c41373aa70c6d5d2669f1bacdc8f07ccc8547
vitramir <vitalis@icedigital.ru>
2020-04-06 20:38:25 +0300

    remove unused import

--- packages/acl/src/modelField.ts
+++ packages/acl/src/modelField.ts
@@ -1,53 +1,15 @@
+import { AMModelType } from '@apollo-model/core/lib/definitions';
+import { AMInputTypeFactory, defaultConfig } from '@apollo-model/core/src';
+import { GraphQLSchema } from 'graphql';
 import R from 'ramda';
-
+import { ACLRule } from './definitions';
 import {
-  GraphQLInputObjectType,
-  GraphQLSchema,
-  GraphQLNamedType,
-  GraphQLField,
-  GraphQLType,
-  GraphQLInputType,
-  GraphQLScalarType,
-  GraphQLEnumType,
-} from 'graphql';
-
-import { AMCreateTypeFactory } from '@apollo-model/core/lib/inputTypes/create';
-import { AMCreateManyNestedTypeFactory } from '@apollo-model/core/lib/inputTypes/createManyNested';
-import { AMCreateManyRelationTypeFactory } from '@apollo-model/core/lib/inputTypes/createManyRelation';
-import { AMCreateOneNestedTypeFactory } from '@apollo-model/core/lib/inputTypes/createOneNested';
-import { AMCreateOneRelationTypeFactory } from '@apollo-model/core/lib/inputTypes/createOneRelation';
-import { AMCreateOneRequiredRelationTypeFactory } from '@apollo-model/core/lib/inputTypes/createOneRequiredRelation';
-import { AMInterfaceCreateTypeFactory } from '@apollo-model/core/lib/inputTypes/interfaceCreate';
-import { AMInterfaceWhereTypeFactory } from '@apollo-model/core/lib/inputTypes/interfaceWhere';
-import { AMInterfaceWhereUniqueTypeFactory } from '@apollo-model/core/lib/inputTypes/interfaceWhereUnique';
-import { AMOrderByTypeFactory } from '@apollo-model/core/lib/inputTypes/orderBy';
-import { AMUpdateTypeFactory } from '@apollo-model/core/lib/inputTypes/update';
-import { AMUpdateManyNestedTypeFactory } from '@apollo-model/core/lib/inputTypes/updateManyNested';
-import { AMUpdateManyRelationTypeFactory } from '@apollo-model/core/lib/inputTypes/updateManyRelation';
-import { AMUpdateOneNestedTypeFactory } from '@apollo-model/core/lib/inputTypes/updateOneNested';
-import { AMUpdateOneRelationTypeFactory } from '@apollo-model/core/lib/inputTypes/updateOneRelation';
-import { AMUpdateWithWhereNestedTypeFactory } from '@apollo-model/core/lib/inputTypes/updateWithWhereNested';
-import { AMWhereTypeFactory } from '@apollo-model/core/lib/inputTypes/where';
-import { AMWhereACLTypeFactory } from '@apollo-model/core/lib/inputTypes/whereACL';
-import { AMWhereCleanTypeFactory } from '@apollo-model/core/lib/inputTypes/whereClean';
-import { AMWhereUniqueTypeFactory } from '@apollo-model/core/lib/inputTypes/whereUnique';
-import {
-  AMModelType,
-  IAMTypeFactory,
-} from '@apollo-model/core/lib/definitions';
-import {
-  matchingTypes,
   extractAbstractTypes,
   matchingFields,
+  matchingTypes,
   toEntries,
   toMap,
 } from './utils';
-import { ACLRule } from './definitions';
-import {
-  defaultConfig,
-  AMTypeFactory,
-  AMInputTypeFactory,
-} from '@apollo-model/core/src';
 
 function transformAccessToInputTypeFactories(
   access: string
