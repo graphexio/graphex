4768108de68f07c85fbafb1ed7d517d91712d948
gregra81 <rashkevitch@gmail.com>
2021-05-24 17:12:12 +0300

    don't run seeders if they were already ran

--- database/seeders/02-post.ts
+++ database/seeders/02-post.ts
@@ -2,6 +2,15 @@ import { QueryInterface } from 'sequelize';
 
 module.exports = {
   up: async (queryInterface: QueryInterface) => {
+    // test if seed was already ran
+    await queryInterface.sequelize
+      .query('SELECT count(*) FROM post')
+      .then(res => {
+        if (res.length > 0) {
+          throw new Error('Seed for post was already ran');
+        }
+      });
+
     const transaction = await queryInterface.sequelize.transaction();
     try {
       await queryInterface.bulkInsert(
